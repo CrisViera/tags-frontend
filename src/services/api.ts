5f51c3ee8b76f0ce2bb99320c19e937118a41e32
CrisViera <cristianvieracruz@gmail.com>
2025-08-17 20:08:09 +0100

    Resolver error en config.headers

--- src/services/api.ts
+++ src/services/api.ts
@@ -11,7 +11,7 @@ api.interceptors.request.use((config) => {
   const token = Cookies.get('XSRF-TOKEN')
   if (token) {
     if (!config.headers) {
-      config.headers = {};
+      config.headers = {} as import('axios').AxiosRequestHeaders;
     }
     config.headers['X-XSRF-TOKEN'] = token;
   }
